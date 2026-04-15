b70675ab0a510b8b2a8eff817ef68eaf028514b4
Kaisar Rayfa Al Baihaqqi <ryf.kaisaaru@gmail.com>
2026-04-15 14:37:42 +0700

    update: certificate arkavidia

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -12,6 +12,12 @@ const certificates = [
     date: "2025",
     image: "/certificates/adikara2025.png",
   },
+  {
+    title: "Participant – Arkavidia 10.0 2026",
+    issuer: "Arkavidia",
+    date: "2026",
+    image: "/certificates/arkavidia2026.jpg",
+  },
 ];
 
 const container = {
